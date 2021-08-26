9a1192ca6d086ab4be3d00653d119cd961dc3b3e
beagle <chldusdn20@gmail.com>
2021-08-26 13:47:19 +0900

    refactor: Refactor chapter map prefab and controller script
    Change the way the button is imported from serializefield in chapter map prefab controller. Because this script has serializefield per stage button, stage buttons are processed one by one, but in changed scripts, they are processed at once.

--- Scripts/ChapterScripts/ChapterMapController.cs
+++ Scripts/ChapterScripts/ChapterMapController.cs
@@ -8,54 +8,25 @@ namespace ChapterScripts
 {
     public class ChapterMapController: MonoBehaviour
     {
-        [SerializeField] private GameObject stage1Button;
-        [SerializeField] private GameObject stage2Button;
-        [SerializeField] private GameObject stage3Button;
-        [SerializeField] private GameObject stage4Button;
-        [SerializeField] private GameObject stage5Button;
-        [SerializeField] private GameObject stage6Button;
+        [SerializeField] private Button[] stageButtons;
         
         public void SetButtonScore(Func<StageType, int> getScore)
         {
-            stage1Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage1);
-            stage2Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage2);
-            stage3Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage3);
-            stage4Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage4);
-            stage5Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage5);
-            stage6Button.GetComponent<StageSpotButtonController>().StageScore = getScore(StageType.Stage6);
+            for (var i = 0; i < stageButtons.Length; i++)
+            {
+                stageButtons[i].GetComponent<StageSpotButtonController>().StageScore = getScore((StageType) i);
+            }
         }
 
         public void SetStageButtonEvent(Action<StageType> openStageReadyPopupAction)
         {
-            stage1Button.GetComponent<Button>().onClick.AddListener(() =>
+            for (var i = 0; i < stageButtons.Length; i++)
             {
-                openStageReadyPopupAction.Invoke(StageType.Stage1);
-            });
-            
-            stage2Button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                openStageReadyPopupAction.Invoke(StageType.Stage2);
-            });
-            
-            stage3Button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                openStageReadyPopupAction.Invoke(StageType.Stage3);
-            });
-            
-            stage4Button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                openStageReadyPopupAction.Invoke(StageType.Stage4);
-            });
-            
-            stage5Button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                openStageReadyPopupAction.Invoke(StageType.Stage5);
-            });
-            
-            stage6Button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                openStageReadyPopupAction.Invoke(StageType.Stage6);
-            });
+                stageButtons[i].onClick.AddListener(() =>
+                {
+                    openStageReadyPopupAction.Invoke((StageType) i);
+                });
+            }
         }   
     }
 }
