a44ad37eb19be8a299855c350903c01a405534cb
beagle <chldusdn20@gmail.com>
2021-08-27 12:57:49 +0900

    refactor: remove method not used

--- Scripts/Util/NodeUtils.cs
+++ Scripts/Util/NodeUtils.cs
@@ -10,12 +10,5 @@ namespace Util
         {
             return Math.Abs(position.x - comparison.x) < 0.05f && Math.Abs(position.y - comparison.y) < 0.05f;
         }
-
-        public static List<Vector3> AddList(this List<Vector3> storage, List<Vector3> newList)
-        {
-            if (newList.Count == 0) return storage;
-            storage.AddRange(newList);
-            return storage;
-        }
     }
 }
