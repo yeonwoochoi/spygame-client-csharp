e42a37a03b599188fea30a6eb7d3269138bd8747
beagle <chldusdn20@gmail.com>
2021-08-27 13:07:49 +0900

    rename: remove unused codes

--- Scripts/UI/Base/BasePopupBehavior.cs
+++ Scripts/UI/Base/BasePopupBehavior.cs
@@ -7,19 +7,19 @@ using DG.Tweening;
 
 namespace UI.Base
 {
-    public enum PopupTweenType
+    public enum PopupMoveType
     {
-        BottomToTop, TopToBottom, RightToLeft, LeftToRight, ScaleUp
+        BottomToTop, TopToBottom, RightToLeft, LeftToRight
     }
 
     public class BasePopupBehavior: BaseUIBehavior
     {
         [SerializeField] protected Text titleText;
-        [SerializeField] protected PopupTweenType tweenType;
+        [SerializeField] protected PopupMoveType moveType;
         private RectTransform popupUITransform;
         
-        protected Vector3 initPosition;
-        protected float moveSpeed = 10f;
+        private Vector3 initPosition;
+        private float moveSpeed = 10f;
         private int initPositionValue = 2000;
 
         protected override void Start()
@@ -28,18 +28,18 @@ namespace UI.Base
             popupUITransform = GetComponent<RectTransform>();
 
             
-            switch (tweenType)
+            switch (moveType)
             {
-                case PopupTweenType.BottomToTop:
+                case PopupMoveType.BottomToTop:
                     initPosition = new Vector3(0, -initPositionValue, 0);
                     break;
-                case PopupTweenType.TopToBottom:
+                case PopupMoveType.TopToBottom:
                     initPosition = new Vector3(0, initPositionValue, 0);
                     break;
-                case PopupTweenType.RightToLeft:
+                case PopupMoveType.RightToLeft:
                     initPosition = new Vector3(initPositionValue, 0, 0);
                     break;
-                case PopupTweenType.LeftToRight:
+                case PopupMoveType.LeftToRight:
                     initPosition = new Vector3(-initPositionValue, 0, 0);
                     break;
                 default:
@@ -48,34 +48,6 @@ namespace UI.Base
 
             popupUITransform.localPosition = initPosition;
 
-            
-            
-            /*
-            initPosition = new Vector3(960, 540, 0);
-            
-            switch (tweenType)
-            {
-                case PopupTweenType.ScaleUp:
-                    break;
-                case PopupTweenType.BottomToTop:
-                    initPosition += new Vector3(0, -initPositionValue, 0);
-                    break;
-                case PopupTweenType.TopToBottom:
-                    initPosition += new Vector3(0, initPositionValue, 0);
-                    break;
-                case PopupTweenType.LeftToRight:    
-                    initPosition += new Vector3(-initPositionValue, 0, 0);
-                    break;
-                case PopupTweenType.RightToLeft:
-                    initPosition += new Vector3(initPositionValue, 0, 0);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            
-            popupUITransform.position = initPosition;
-            */
-            
             isOpen = false;
         }
 
@@ -91,15 +63,13 @@ namespace UI.Base
             ActivateUI();
             while (isOpen)
             {
-                if (tweenType == PopupTweenType.TopToBottom || tweenType == PopupTweenType.BottomToTop)
+                if (moveType == PopupMoveType.TopToBottom || moveType == PopupMoveType.BottomToTop)
                 {
                     popupUITransform.localPosition = new Vector3(0, Mathf.Lerp(popupUITransform.localPosition.y, 0, Time.deltaTime * moveSpeed), 0);
-                    // DoTweenByType(false);
                 }
                 else
                 {
                     popupUITransform.localPosition = new Vector3(Mathf.Lerp(popupUITransform.localPosition.x, 0, Time.deltaTime * moveSpeed), 0, 0);
-                    // DoTweenByType(false);
                 }
                 
                 yield return null;
@@ -124,57 +94,19 @@ namespace UI.Base
             ActivateUI(false);
             while (!isOpen)
             {
-                if (tweenType == PopupTweenType.TopToBottom || tweenType == PopupTweenType.BottomToTop)
+                if (moveType == PopupMoveType.TopToBottom || moveType == PopupMoveType.BottomToTop)
                 {
                     popupUITransform.localPosition = new Vector3(0, Mathf.Lerp(popupUITransform.localPosition.y, initPosition.y, Time.deltaTime * moveSpeed), 0);
-                    //DoTweenByType(true);
                 }
                 else
                 {
                     popupUITransform.localPosition = new Vector3(Mathf.Lerp(popupUITransform.localPosition.x, initPosition.x, Time.deltaTime * moveSpeed), 0, 0);
-                    //DoTweenByType(true);
                 }
                 
                 yield return null;
                 if (!(Vector3.Distance(popupUITransform.position, initPosition) <= 0.001f)) continue;
                 ResetAll();
                 yield break;
-            }
-        }
-        
-        private void DoTweenByType(bool isReverse) {
-            var openEase = Ease.Linear;
-            var closeEase = Ease.Linear;
-            switch (tweenType) {
-                case PopupTweenType.ScaleUp:
-                    if (!isReverse)
-                        DOTween.Sequence().Append(popupUITransform.DOScale(new Vector3(1.1f, 1.1f), 0.2f).SetEase(Ease.InQuart))
-                            .Append(popupUITransform.DOScale(Vector3.one, 0.2f));
-                    break;
-                case PopupTweenType.TopToBottom:
-                    if (isReverse)
-                        popupUITransform.DOAnchorPosY(initPositionValue, 0.25f).SetEase(closeEase);
-                    else
-                        popupUITransform.DOAnchorPosY(0, 0.5f).SetEase(openEase);
-                    break;
-                case PopupTweenType.BottomToTop:
-                    if (isReverse)
-                        popupUITransform.DOAnchorPosY(-initPositionValue, 0.25f).SetEase(closeEase);
-                    else
-                        popupUITransform.DOAnchorPosY(0, 0.5f).SetEase(openEase);
-                    break;
-                case PopupTweenType.LeftToRight:
-                    if (isReverse)
-                        popupUITransform.DOAnchorPosX(-initPositionValue, 0.25f).SetEase(closeEase);
-                    else
-                        popupUITransform.DOAnchorPosX(0, 0.25f).SetEase(openEase);
-                    break;
-                case PopupTweenType.RightToLeft:
-                    if (isReverse)
-                        popupUITransform.DOAnchorPosX(initPositionValue, 0.25f).SetEase(closeEase);
-                    else
-                        popupUITransform.DOAnchorPosX(0, 0.25f).SetEase(openEase);
-                    break;
             }
         }
         
